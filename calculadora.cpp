074b0fb20117ccc5775f0fdba3557320a1b5c3ce
Diegoearevalo <00367725@uca.edu.sv>
2026-01-09 16:47:25 -0600

    add: ingreso de datos

---
+++ calculadora.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operacion;
+    double n1, n2;
+
+    cout << "Calculadora Sencilla" << endl;
+    
+    cout << "Elige una operacion (+, -, *, /): ";
+    cin >> operacion;
+
+    cout << "Introduce el primer numero: ";
+    cin >> n1;
+    cout << "Introduce el segundo numero: ";
+    cin >> n2;
+
+}
