c9f66ebaa7a57c0ad5dece098551c82148c8862d
Gersonarbol <00360925uca.edu.sv>
2026-01-09 16:57:29 -0600

    add: operaciones basicas y menu

--- calculadora.cpp
+++ calculadora.cpp
@@ -16,4 +16,27 @@ int main() {
     cout << "Introduce el segundo numero: ";
     cin >> n2;
 
+    switch(operacion) {
+        case '+':
+            cout << "Resultado: " << n1 + n2 << endl;
+            break;
+        case '-':
+            cout << "Resultado: " << n1 - n2 << endl;
+            break;
+        case '*':
+            cout << "Resultado: " << n1 * n2 << endl;
+            break;
+        case '/':
+            if (n2 != 0) {
+                cout << "Resultado: " << n1 / n2 << endl;
+            } else {
+                cout << "No se puede dividir entre cero." << endl;
+            }
+            break;
+        default:
+            cout << "Operacion no valida." << endl;
+            break;
+    }
+
+    return 0;
 }
